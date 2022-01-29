b92128d3dd0e0279ecb05b5d457f95f66f4f1f77
santosy <Yuri.Santos@t-systems.com>
2022-01-29 15:57:04 -0300

    created endpoint save person

--- src/main/java/one/digitalinnovation/personapi/entity/Phone.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Phone {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
