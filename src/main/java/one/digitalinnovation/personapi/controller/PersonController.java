023d9a69a3831ca0c0ec19583494e3bdb1bf23be
santosy <Yuri.Santos@t-systems.com>
2022-01-31 17:00:08 -0300

    new feature endpoint get people

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 @RestController
@@ -24,9 +25,8 @@ public class PersonController {
         return personService.create(personDTO);
     }
 
-    @GetMapping("/test")
-    @ResponseStatus(HttpStatus.CREATED)
-    public String createTest() {
-        return "Test";
+    @GetMapping
+    public List<PersonDTO> listAll(){
+        return personService.listAll();
     }
 }
